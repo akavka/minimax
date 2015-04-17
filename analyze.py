9b1b69f9fed318d50faf159a919b9101c65c9762
Adam Kavka <akavka@unix1.andrew.cmu.edu>
2015-04-16 21:31:13 -0400

    added file comparison to analyze

--- analyze.py
+++ analyze.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python
+import sys
+def compareGames(inFile1, inFile2):
+    in1=open(inFile1, "r")
+    in2=open(inFile2,"r")
+    #result=True
+    lines1=in1.readlines()
+    lines2=in2.readlines()
+    if (len(lines1)!=len(lines2)):
+        return False
+    
+    #implicit else
+    for i in range(len(lines1)):
+
+        if lines1[i]!=lines2[i]:
+            return False
+    return True
 
 
-inFile=open("time.dat", "r");
-lines=inFile.readlines();
-gameIndex=0
-numTurns=0
-mySum=0
-for line in lines:
-    if line[0]==("g"):
-        print("Average time was "+str(mySum/numTurns) + " for game " + str(gameIndex))
-        numTurns=0
-        mySum=0
-        gameIndex+=1
-    else:
-        mySum+=float(line)
-        numTurns+=1
-inFile.close();
+def main():
+    print("Comparing "+sys.argv[1]+ " and " + sys.argv[2] + ":\n")
+    print(str(compareGames(sys.argv[1], sys.argv[2])))
+
+    inFile=open("time.dat", "r");
+    lines=inFile.readlines();
+    gameIndex=0
+    numTurns=0
+    mySum=0
+    for line in lines:
+        if line[0]==("g"):
+            print("Average time was "+str(mySum/numTurns) + " for game " + str(gameIndex))
+            numTurns=0
+            mySum=0
+            gameIndex+=1
+        else:
+            mySum+=float(line)
+            numTurns+=1
+    inFile.close();
+
+if __name__=="__main__":
+    main()
