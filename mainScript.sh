d7cd177305e80f6f4861d01d7df1d38619f9e992
Adam Kavka <akavka@unix6.andrew.cmu.edu>
2015-04-24 19:09:32 -0400

    reverted back to state that works with friend, enemy, black, white. Also changed some scripts to only run one game and not write errors to file.

--- mainScript.sh
+++ mainScript.sh
@@ -7,6 +7,6 @@ gcc -fcilkplus -lcilkrts -o mscp mscp.c
 
 echo "Did first run."
 
-./mscp $1 1  < inputBoth.txt > out2.txt 2>error.txt
+./mscp $1 1  < inputBoth.txt > out2.txt
 
 python analyze.py out1.txt out2.txt
