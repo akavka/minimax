e905db7318ff07cae8f1c635270d8501825e890d
akavka <akavka@latedays.andrew.cmu.edu>
2015-05-07 15:35:35 -0400

    fixed bulkScript

--- bulkScript.sh
+++ bulkScript.sh
@@ -1,216 +1,217 @@
 #!/bin/bash
-
+depth=7
+numTurns=13
 if [1 -eq 0 ]; then
-mkdir safe_7470_1_7_13
-qsub latedays.qsub -v arg1=7470,arg2=1,arg3=7,arg4=13,arg5=safe_7470_1_7_13/
+mkdir safe_7470_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=1,arg3=7,arg4=13,arg5=safe_7470_1_"%depth"_"$numTurns"/
 
-mkdir safe_7471_1_7_13
-qsub latedays.qsub -v arg1=7471,arg2=1,arg3=7,arg4=13,arg5=safe_7471_1_7_13/
+mkdir safe_7471_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=1,arg3=7,arg4=13,arg5=safe_7471_1_"%depth"_"$numTurns"/
 
-mkdir safe_7472_1_7_13
-qsub latedays.qsub -v arg1=7472,arg2=1,arg3=7,arg4=13,arg5=safe_7472_1_7_13/
+mkdir safe_7472_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=1,arg3=7,arg4=13,arg5=safe_7472_1_"%depth"_"$numTurns"/
 
-mkdir safe_7473_1_7_13
-qsub latedays.qsub -v arg1=7473,arg2=1,arg3=7,arg4=13,arg5=safe_7473_1_7_13/
+mkdir safe_7473_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=1,arg3=7,arg4=13,arg5=safe_7473_1_"%depth"_"$numTurns"/
 
-mkdir safe_7474_1_7_13
-qsub latedays.qsub -v arg1=7474,arg2=1,arg3=7,arg4=13,arg5=safe_7474_1_7_13/
+mkdir safe_7474_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=1,arg3=7,arg4=13,arg5=safe_7474_1_"%depth"_"$numTurns"/
 
-mkdir safe_7475_1_7_13
-qsub latedays.qsub -v arg1=7475,arg2=1,arg3=7,arg4=13,arg5=safe_7475_1_7_13/
+mkdir safe_7475_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=1,arg3=7,arg4=13,arg5=safe_7475_1_"%depth"_"$numTurns"/
 
-mkdir safe_7476_1_7_13
-qsub latedays.qsub -v arg1=7476,arg2=1,arg3=7,arg4=13,arg5=safe_7476_1_7_13/
+mkdir safe_7476_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=1,arg3=7,arg4=13,arg5=safe_7476_1_"%depth"_"$numTurns"/
 
-mkdir safe_7477_1_7_13
-qsub latedays.qsub -v arg1=7477,arg2=1,arg3=7,arg4=13,arg5=safe_7477_1_7_13/
+mkdir safe_7477_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=1,arg3=7,arg4=13,arg5=safe_7477_1_"%depth"_"$numTurns"/
 
-mkdir safe_7478_1_7_13
-qsub latedays.qsub -v arg1=7478,arg2=1,arg3=7,arg4=13,arg5=safe_7478_1_7_13/
+mkdir safe_7478_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=1,arg3=7,arg4=13,arg5=safe_7478_1_"%depth"_"$numTurns"/
 
-mkdir safe_7479_1_7_13
-qsub latedays.qsub -v arg1=7479,arg2=1,arg3=7,arg4=13,arg5=safe_7479_1_7_13/
-
-
- mkdir safe_7470_2_7_13
-qsub latedays.qsub -v arg1=7470,arg2=2,arg3=7,arg4=13,arg5=safe_7470_2_7_13/
-
-mkdir safe_7471_2_7_13
-qsub latedays.qsub -v arg1=7471,arg2=2,arg3=7,arg4=13,arg5=safe_7471_2_7_13/
-
-mkdir safe_7472_2_7_13
-qsub latedays.qsub -v arg1=7472,arg2=2,arg3=7,arg4=13,arg5=safe_7472_2_7_13/
-
-mkdir safe_7473_2_7_13
-qsub latedays.qsub -v arg1=7473,arg2=2,arg3=7,arg4=13,arg5=safe_7473_2_7_13/
-
-mkdir safe_7474_2_7_13
-qsub latedays.qsub -v arg1=7474,arg2=2,arg3=7,arg4=13,arg5=safe_7474_2_7_13/
-
-mkdir safe_7475_2_7_13
-qsub latedays.qsub -v arg1=7475,arg2=2,arg3=7,arg4=13,arg5=safe_7475_2_7_13/
-
-mkdir safe_7476_2_7_13
-qsub latedays.qsub -v arg1=7476,arg2=2,arg3=7,arg4=13,arg5=safe_7476_2_7_13/
-
-mkdir safe_7477_2_7_13
-qsub latedays.qsub -v arg1=7477,arg2=2,arg3=7,arg4=13,arg5=safe_7477_2_7_13/
-
-mkdir safe_7478_2_7_13
-qsub latedays.qsub -v arg1=7478,arg2=2,arg3=7,arg4=13,arg5=safe_7478_2_7_13/
-
-mkdir safe_7479_2_7_13
-qsub latedays.qsub -v arg1=7479,arg2=2,arg3=7,arg4=13,arg5=safe_7479_2_7_13/
-
-mkdir safe_7470_3_7_13
-qsub latedays.qsub -v arg1=7470,arg2=3,arg3=7,arg4=13,arg5=safe_7470_3_7_13/
-
-mkdir safe_7471_3_7_13
-qsub latedays.qsub -v arg1=7471,arg2=3,arg3=7,arg4=13,arg5=safe_7471_3_7_13/
-
-mkdir safe_7472_3_7_13
-qsub latedays.qsub -v arg1=7472,arg2=3,arg3=7,arg4=13,arg5=safe_7472_3_7_13/
-
-mkdir safe_7473_3_7_13
-qsub latedays.qsub -v arg1=7473,arg2=3,arg3=7,arg4=13,arg5=safe_7473_3_7_13/
-
-mkdir safe_7474_3_7_13
-qsub latedays.qsub -v arg1=7474,arg2=3,arg3=7,arg4=13,arg5=safe_7474_3_7_13/
-
-mkdir safe_7475_3_7_13
-qsub latedays.qsub -v arg1=7475,arg2=3,arg3=7,arg4=13,arg5=safe_7475_3_7_13/
-
-mkdir safe_7476_3_7_13
-qsub latedays.qsub -v arg1=7476,arg2=3,arg3=7,arg4=13,arg5=safe_7476_3_7_13/
-
-mkdir safe_7477_3_7_13
-qsub latedays.qsub -v arg1=7477,arg2=3,arg3=7,arg4=13,arg5=safe_7477_3_7_13/
-
-mkdir safe_7478_3_7_13
-qsub latedays.qsub -v arg1=7478,arg2=3,arg3=7,arg4=13,arg5=safe_7478_3_7_13/
-
-mkdir safe_7479_3_7_13
-qsub latedays.qsub -v arg1=7479,arg2=3,arg3=7,arg4=13,arg5=safe_7479_3_7_13/
+mkdir safe_7479_1_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=1,arg3=7,arg4=13,arg5=safe_7479_1_"%depth"_"$numTurns"/
 
 
-mkdir safe_7470_4_7_13
-qsub latedays.qsub -v arg1=7470,arg2=4,arg3=7,arg4=13,arg5=safe_7470_4_7_13/
+ mkdir safe_7470_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=2,arg3=7,arg4=13,arg5=safe_7470_2_"%depth"_"$numTurns"/
 
-mkdir safe_7471_4_7_13
-qsub latedays.qsub -v arg1=7471,arg2=4,arg3=7,arg4=13,arg5=safe_7471_4_7_13/
+mkdir safe_7471_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=2,arg3=7,arg4=13,arg5=safe_7471_2_"%depth"_"$numTurns"/
 
-mkdir safe_7472_4_7_13
-qsub latedays.qsub -v arg1=7472,arg2=4,arg3=7,arg4=13,arg5=safe_7472_4_7_13/
+mkdir safe_7472_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=2,arg3=7,arg4=13,arg5=safe_7472_2_"%depth"_"$numTurns"/
 
-mkdir safe_7473_4_7_13
-qsub latedays.qsub -v arg1=7473,arg2=4,arg3=7,arg4=13,arg5=safe_7473_4_7_13/
+mkdir safe_7473_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=2,arg3=7,arg4=13,arg5=safe_7473_2_"%depth"_"$numTurns"/
 
-mkdir safe_7474_4_7_13
-qsub latedays.qsub -v arg1=7474,arg2=4,arg3=7,arg4=13,arg5=safe_7474_4_7_13/
+mkdir safe_7474_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=2,arg3=7,arg4=13,arg5=safe_7474_2_"%depth"_"$numTurns"/
 
-mkdir safe_7475_4_7_13
-qsub latedays.qsub -v arg1=7475,arg2=4,arg3=7,arg4=13,arg5=safe_7475_4_7_13/
+mkdir safe_7475_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=2,arg3=7,arg4=13,arg5=safe_7475_2_"%depth"_"$numTurns"/
 
-mkdir safe_7476_4_7_13
-qsub latedays.qsub -v arg1=7476,arg2=4,arg3=7,arg4=13,arg5=safe_7476_4_7_13/
+mkdir safe_7476_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=2,arg3=7,arg4=13,arg5=safe_7476_2_"%depth"_"$numTurns"/
 
-mkdir safe_7477_4_7_13
-qsub latedays.qsub -v arg1=7477,arg2=4,arg3=7,arg4=13,arg5=safe_7477_4_7_13/
+mkdir safe_7477_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=2,arg3=7,arg4=13,arg5=safe_7477_2_"%depth"_"$numTurns"/
 
-mkdir safe_7478_4_7_13
-qsub latedays.qsub -v arg1=7478,arg2=4,arg3=7,arg4=13,arg5=safe_7478_4_7_13/
+mkdir safe_7478_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=2,arg3=7,arg4=13,arg5=safe_7478_2_"%depth"_"$numTurns"/
 
-mkdir safe_7479_4_7_13
-qsub latedays.qsub -v arg1=7479,arg2=4,arg3=7,arg4=13,arg5=safe_7479_4_7_13/
+mkdir safe_7479_2_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=2,arg3=7,arg4=13,arg5=safe_7479_2_"%depth"_"$numTurns"/
 
-mkdir safe_7470_8_7_13
-qsub latedays.qsub -v arg1=7470,arg2=8,arg3=7,arg4=13,arg5=safe_7470_8_7_13/
+mkdir safe_7470_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=3,arg3=7,arg4=13,arg5=safe_7470_3_"%depth"_"$numTurns"/
 
-mkdir safe_7471_8_7_13
-qsub latedays.qsub -v arg1=7471,arg2=8,arg3=7,arg4=13,arg5=safe_7471_8_7_13/
+mkdir safe_7471_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=3,arg3=7,arg4=13,arg5=safe_7471_3_"%depth"_"$numTurns"/
 
-mkdir safe_7472_8_7_13
-qsub latedays.qsub -v arg1=7472,arg2=8,arg3=7,arg4=13,arg5=safe_7472_8_7_13/
+mkdir safe_7472_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=3,arg3=7,arg4=13,arg5=safe_7472_3_"%depth"_"$numTurns"/
 
-mkdir safe_7473_8_7_13
-qsub latedays.qsub -v arg1=7473,arg2=8,arg3=7,arg4=13,arg5=safe_7473_8_7_13/
+mkdir safe_7473_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=3,arg3=7,arg4=13,arg5=safe_7473_3_"%depth"_"$numTurns"/
 
-mkdir safe_7474_8_7_13
-qsub latedays.qsub -v arg1=7474,arg2=8,arg3=7,arg4=13,arg5=safe_7474_8_7_13/
+mkdir safe_7474_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=3,arg3=7,arg4=13,arg5=safe_7474_3_"%depth"_"$numTurns"/
 
-mkdir safe_7475_8_7_13
-qsub latedays.qsub -v arg1=7475,arg2=8,arg3=7,arg4=13,arg5=safe_7475_8_7_13/
+mkdir safe_7475_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=3,arg3=7,arg4=13,arg5=safe_7475_3_"%depth"_"$numTurns"/
 
-mkdir safe_7476_8_7_13
-qsub latedays.qsub -v arg1=7476,arg2=8,arg3=7,arg4=13,arg5=safe_7476_8_7_13/
+mkdir safe_7476_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=3,arg3=7,arg4=13,arg5=safe_7476_3_"%depth"_"$numTurns"/
 
-mkdir safe_7477_8_7_13
-qsub latedays.qsub -v arg1=7477,arg2=8,arg3=7,arg4=13,arg5=safe_7477_8_7_13/
+mkdir safe_7477_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=3,arg3=7,arg4=13,arg5=safe_7477_3_"%depth"_"$numTurns"/
 
-mkdir safe_7478_8_7_13
-qsub latedays.qsub -v arg1=7478,arg2=8,arg3=7,arg4=13,arg5=safe_7478_8_7_13/
+mkdir safe_7478_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=3,arg3=7,arg4=13,arg5=safe_7478_3_"%depth"_"$numTurns"/
 
-mkdir safe_7479_8_7_13
-qsub latedays.qsub -v arg1=7479,arg2=8,arg3=7,arg4=13,arg5=safe_7479_8_7_13/
+mkdir safe_7479_3_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=3,arg3=7,arg4=13,arg5=safe_7479_3_"%depth"_"$numTurns"/
 
-mkdir safe_7470_12_7_13
-qsub latedays.qsub -v arg1=7470,arg2=12,arg3=7,arg4=13,arg5=safe_7470_12_7_13/
 
-mkdir safe_7471_12_7_13
-qsub latedays.qsub -v arg1=7471,arg2=12,arg3=7,arg4=13,arg5=safe_7471_12_7_13/
+mkdir safe_7470_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=4,arg3=7,arg4=13,arg5=safe_7470_4_"%depth"_"$numTurns"/
 
-mkdir safe_7472_12_7_13
-qsub latedays.qsub -v arg1=7472,arg2=12,arg3=7,arg4=13,arg5=safe_7472_12_7_13/
+mkdir safe_7471_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=4,arg3=7,arg4=13,arg5=safe_7471_4_"%depth"_"$numTurns"/
 
-mkdir safe_7473_12_7_13
-qsub latedays.qsub -v arg1=7473,arg2=12,arg3=7,arg4=13,arg5=safe_7473_12_7_13/
+mkdir safe_7472_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=4,arg3=7,arg4=13,arg5=safe_7472_4_"%depth"_"$numTurns"/
 
-mkdir safe_7474_12_7_13
-qsub latedays.qsub -v arg1=7474,arg2=12,arg3=7,arg4=13,arg5=safe_7474_12_7_13/
+mkdir safe_7473_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=4,arg3=7,arg4=13,arg5=safe_7473_4_"%depth"_"$numTurns"/
 
-mkdir safe_7475_12_7_13
-qsub latedays.qsub -v arg1=7475,arg2=12,arg3=7,arg4=13,arg5=safe_7475_12_7_13/
+mkdir safe_7474_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=4,arg3=7,arg4=13,arg5=safe_7474_4_"%depth"_"$numTurns"/
 
-mkdir safe_7476_12_7_13
-qsub latedays.qsub -v arg1=7476,arg2=12,arg3=7,arg4=13,arg5=safe_7476_12_7_13/
+mkdir safe_7475_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=4,arg3=7,arg4=13,arg5=safe_7475_4_"%depth"_"$numTurns"/
 
-mkdir safe_7477_12_7_13
-qsub latedays.qsub -v arg1=7477,arg2=12,arg3=7,arg4=13,arg5=safe_7477_12_7_13/
+mkdir safe_7476_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=4,arg3=7,arg4=13,arg5=safe_7476_4_"%depth"_"$numTurns"/
 
-mkdir safe_7478_12_7_13
-qsub latedays.qsub -v arg1=7478,arg2=12,arg3=7,arg4=13,arg5=safe_7478_12_7_13/
+mkdir safe_7477_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=4,arg3=7,arg4=13,arg5=safe_7477_4_"%depth"_"$numTurns"/
 
-mkdir safe_7479_12_7_13
-qsub latedays.qsub -v arg1=7479,arg2=12,arg3=7,arg4=13,arg5=safe_7479_12_7_13/
+mkdir safe_7478_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=4,arg3=7,arg4=13,arg5=safe_7478_4_"%depth"_"$numTurns"/
+
+mkdir safe_7479_4_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=4,arg3=7,arg4=13,arg5=safe_7479_4_"%depth"_"$numTurns"/
+
+mkdir safe_7470_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=8,arg3=7,arg4=13,arg5=safe_7470_8_"%depth"_"$numTurns"/
+
+mkdir safe_7471_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=8,arg3=7,arg4=13,arg5=safe_7471_8_"%depth"_"$numTurns"/
+
+mkdir safe_7472_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=8,arg3=7,arg4=13,arg5=safe_7472_8_"%depth"_"$numTurns"/
+
+mkdir safe_7473_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=8,arg3=7,arg4=13,arg5=safe_7473_8_"%depth"_"$numTurns"/
+
+mkdir safe_7474_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=8,arg3=7,arg4=13,arg5=safe_7474_8_"%depth"_"$numTurns"/
+
+mkdir safe_7475_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=8,arg3=7,arg4=13,arg5=safe_7475_8_"%depth"_"$numTurns"/
+
+mkdir safe_7476_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=8,arg3=7,arg4=13,arg5=safe_7476_8_"%depth"_"$numTurns"/
+
+mkdir safe_7477_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=8,arg3=7,arg4=13,arg5=safe_7477_8_"%depth"_"$numTurns"/
+
+mkdir safe_7478_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=8,arg3=7,arg4=13,arg5=safe_7478_8_"%depth"_"$numTurns"/
+
+mkdir safe_7479_8_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=8,arg3=7,arg4=13,arg5=safe_7479_8_"%depth"_"$numTurns"/
+
+mkdir safe_7470_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=12,arg3=7,arg4=13,arg5=safe_7470_12_"%depth"_"$numTurns"/
+
+mkdir safe_7471_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=12,arg3=7,arg4=13,arg5=safe_7471_12_"%depth"_"$numTurns"/
+
+mkdir safe_7472_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=12,arg3=7,arg4=13,arg5=safe_7472_12_"%depth"_"$numTurns"/
+
+mkdir safe_7473_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=12,arg3=7,arg4=13,arg5=safe_7473_12_"%depth"_"$numTurns"/
+
+mkdir safe_7474_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=12,arg3=7,arg4=13,arg5=safe_7474_12_"%depth"_"$numTurns"/
+
+mkdir safe_7475_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=12,arg3=7,arg4=13,arg5=safe_7475_12_"%depth"_"$numTurns"/
+
+mkdir safe_7476_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=12,arg3=7,arg4=13,arg5=safe_7476_12_"%depth"_"$numTurns"/
+
+mkdir safe_7477_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=12,arg3=7,arg4=13,arg5=safe_7477_12_"%depth"_"$numTurns"/
+
+mkdir safe_7478_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=12,arg3=7,arg4=13,arg5=safe_7478_12_"%depth"_"$numTurns"/
+
+mkdir safe_7479_12_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=12,arg3=7,arg4=13,arg5=safe_7479_12_"%depth"_"$numTurns"/
 
 fi
 
-mkdir safe_7470_24_7_13
-qsub latedays.qsub -v arg1=7470,arg2=24,arg3=7,arg4=13,arg5=safe_7470_24_7_13/
+mkdir safe_7470_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7470,arg2=24,arg3=7,arg4=13,arg5=safe_7470_24_"%depth"_"$numTurns"/
 
-mkdir safe_7471_24_7_13
-qsub latedays.qsub -v arg1=7471,arg2=24,arg3=7,arg4=13,arg5=safe_7471_24_7_13/
+mkdir safe_7471_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7471,arg2=24,arg3=7,arg4=13,arg5=safe_7471_24_"%depth"_"$numTurns"/
 
-mkdir safe_7472_24_7_13
-qsub latedays.qsub -v arg1=7472,arg2=24,arg3=7,arg4=13,arg5=safe_7472_24_7_13/
+mkdir safe_7472_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7472,arg2=24,arg3=7,arg4=13,arg5=safe_7472_24_"%depth"_"$numTurns"/
 
-mkdir safe_7473_24_7_13
-qsub latedays.qsub -v arg1=7473,arg2=24,arg3=7,arg4=13,arg5=safe_7473_24_7_13/
+mkdir safe_7473_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7473,arg2=24,arg3=7,arg4=13,arg5=safe_7473_24_"%depth"_"$numTurns"/
 
-mkdir safe_7474_24_7_13
-qsub latedays.qsub -v arg1=7474,arg2=24,arg3=7,arg4=13,arg5=safe_7474_24_7_13/
+mkdir safe_7474_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7474,arg2=24,arg3=7,arg4=13,arg5=safe_7474_24_"%depth"_"$numTurns"/
 
-mkdir safe_7475_24_7_13
-qsub latedays.qsub -v arg1=7475,arg2=24,arg3=7,arg4=13,arg5=safe_7475_24_7_13/
+mkdir safe_7475_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7475,arg2=24,arg3=7,arg4=13,arg5=safe_7475_24_"%depth"_"$numTurns"/
 
-mkdir safe_7476_24_7_13
-qsub latedays.qsub -v arg1=7476,arg2=24,arg3=7,arg4=13,arg5=safe_7476_24_7_13/
+mkdir safe_7476_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7476,arg2=24,arg3=7,arg4=13,arg5=safe_7476_24_"%depth"_"$numTurns"/
 
-mkdir safe_7477_24_7_13
-qsub latedays.qsub -v arg1=7477,arg2=24,arg3=7,arg4=13,arg5=safe_7477_24_7_13/
+mkdir safe_7477_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7477,arg2=24,arg3=7,arg4=13,arg5=safe_7477_24_"%depth"_"$numTurns"/
 
-mkdir safe_7478_24_7_13
-qsub latedays.qsub -v arg1=7478,arg2=24,arg3=7,arg4=13,arg5=safe_7478_24_7_13/
+mkdir safe_7478_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7478,arg2=24,arg3=7,arg4=13,arg5=safe_7478_24_"%depth"_"$numTurns"/
 
-mkdir safe_7479_24_7_13
-qsub latedays.qsub -v arg1=7479,arg2=24,arg3=7,arg4=13,arg5=safe_7479_24_7_13/
+mkdir safe_7479_24_"%depth"_"$numTurns"
+qsub latedays.qsub -v arg1=7479,arg2=24,arg3=7,arg4=13,arg5=safe_7479_24_"%depth"_"$numTurns"/
