1a6531b34db9ac921e97fadbd9590861680885ef
Adam Kavka <akavka@unix1.andrew.cmu.edu>
2015-05-08 01:42:16 -0400

    successfully added getWinner to analyze.py

--- analyze.py
+++ analyze.py
@@ -2,6 +2,23 @@
 import sys
 #latepath="/home/akavka/minimax/"
 
+def getResult(inFile):
+    wins =0;
+    draws=0;
+    losses=0;
+    instream=open(inFile,"r")
+    lines=instream.readlines()
+    for i in range (len(lines)):
+        if lines[i].startswith("game over: 1-0"):
+            print ("Result loss")
+            
+        if lines[i].startswith("game over: 1/2-1/2"):
+            print ("Result draw")
+        if lines[i].startswith("game over: 0-1"):
+            print("Result win")
+
+
+
 def compareGames(inFile1, inFile2):
     in1=open(inFile1, "r")
     in2=open(inFile2,"r")
@@ -97,7 +114,8 @@ def analyzeDepth(filename, side):
     mySum=0
     numTurns=0
     for line in lines:
-        mySum+=int(line)
+        if int(line)<20:
+            mySum+=int(line)
         numTurns+=1
     if numTurns==0:
         numTurns=1
@@ -107,6 +125,7 @@ def analyzeDepth(filename, side):
 
 def main():
     latepath=sys.argv[4]+sys.argv[5]
+    """
     print("Comparing "+sys.argv[1]+ " and " + sys.argv[2] + ":\n")
     print(str(compareGames(sys.argv[1], sys.argv[2])))
 
@@ -114,7 +133,7 @@ def main():
     totalTime1=analyzeOutput(latepath+"time1.dat")
     totalTime2=analyzeOutput(latepath+"time2.dat")
 
-    """
+    
     firstTime1=analyzeTime(latepath+"time_first1.dat")
     firstTime2=analyzeTime(latepath+"time_first2.dat")
     secondTime1=analyzeTime(latepath+"time_second1.dat")
@@ -126,8 +145,9 @@ def main():
     secondCount1=analyzeCount(latepath+"count_second1.dat")
     secondCount2=analyzeCount(latepath+"count_second2.dat")
     """
-    analyzeDepth(latepath+"real1.dat", "W")
-    analyzeDepth(latepath+"real2.dat", "B")
+    analyzeDepth(latepath+"depth_white.dat", "W")
+    analyzeDepth(latepath+"depth_black.dat", "B")    
+    getResult(latepath+"out2.txt")
 
 #    analyzeDivergence(latepath+"divergence2.dat", sys.argv[3])
     """
